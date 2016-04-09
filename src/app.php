44eef98c8042f8ca6ac12b60f0fb768afd4bffa4
bluelbj23 <yc3079@columbia.edu>
2016-04-08 23:17:45 -0400

    try to show data

--- src/app.php
+++ src/app.php
@@ -21,7 +21,7 @@ $app['db'] = $app->share(function ($app) {
 });
 
 // Handle the index page
-$app->match('/', function () use ($app) {
+$app->match('/fdg', function () use ($app) {
     /*
     $query = $app['db']->prepare("SELECT message, author FROM {$app['db.table']}");
     $thoughts = $query->execute() ? $query->fetchAll(PDO::FETCH_ASSOC) : array();
@@ -36,15 +36,15 @@ $app->match('/', function () use ($app) {
 });
 
 // Handle the add page
-$app->match('/add', function (Request $request) use ($app) {
+$app->match('/', function (Request $request) use ($app) {
     $alert = null;
     // If the form was submitted, process the input
     if ('POST' == $request->getMethod()) {
         try {
             // Make sure the photo was uploaded without error
-            $message = $request->request->get('thoughtMessage');
-            $author = $request->request->get('thoughtAuthor');
-            if ($message && $author && strlen($author) < 64) {
+            $message = $request->request;//->get('thoughtMessage');
+            //$author = $request->request->get('thoughtAuthor');
+            /*if ($message && $author && strlen($author) < 64) {
                 // Save the thought record to the database
                 $sql = "INSERT INTO {$app['db.table']} (message, author) VALUES (:message, :author)";
                 $query = $app['db']->prepare($sql);
@@ -57,19 +57,20 @@ $app->match('/add', function (Request $request) use ($app) {
                 }
             } else {
                 throw new \InvalidArgumentException('Sorry, The format of your thought was not valid.');
-            }
+            }*/
 
             // Display a success message
-            $alert = array('type' => 'success', 'message' => 'Thank you for sharing your thought.');
+            //$alert = array('type' => 'success', 'message' => 'Thank you for sharing your thought.');
         } catch (Exception $e) {
             // Display an error message
             $alert = array('type' => 'error', 'message' => $e->getMessage());
         }
     }
 
-    return $app['twig']->render('add.twig', array(
-        'title' => 'Share Your Thought!',
-        'alert' => $alert,
+    return $app['twig']->render('show.twig', array(
+        //'title' => 'Share Your Thought!',
+        //'alert' => $alert,
+        $message,
     ));
 });
 
